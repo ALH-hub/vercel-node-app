7408c89c58c2af3408cea1ecb02863f7ad168599
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-25 15:35:15 +0100

    test api routes

---
+++ README.md
@@ -0,0 +1,2 @@
+# vercel-node-app
+# vercel-node-app
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Home Page Route'));
+// Add more routes as needed
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+
