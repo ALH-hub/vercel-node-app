72951f197a6d0f14476b82c73968f7bcaa46c32e
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-25 15:41:45 +0100

    test api routes

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Home Page Route'));
 // Add more routes as needed
+
+app.get('/api', (req, res) =>
+	res.json({name: "oumate", age: 15});
+);
+
+app.get('/names', (req, res) => {
+	res.json({
+		{
+			id: 1,
+			name: 'oums',
+			age: '12',
+		},
+		{
+			id: 2,
+			name: 'fred',
+			age: 23,
+		},
+	});
+});
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
